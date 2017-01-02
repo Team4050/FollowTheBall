1b540803b3ba679eafe1dd6904ef58336a4f7ee3
Carson <cds17257@gmail.com>
2017-01-02 12:04:16 -0500

    Added initial project files

---
+++ Robot/src/main/java/frc/team4050/ftb/RobotModule.java
@@ -0,0 +1,25 @@
+package frc.team4050.ftb;
+
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class RobotModule extends IterativeModule {
+
+    public static Logger logger;
+
+    @Override
+    public String getModuleName() {
+        return "FTB";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("FTB", Logger.ATTR_DEFAULT);
+        //TODO: Module Init
+    }
+}
